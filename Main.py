37283fd43dbb20671dcf26f7d0bd6a62f890eefa
sDriskell <49995352+sDriskell@users.noreply.github.com>
2019-11-16 12:57:24 -0500

    Checking Dates
    Program now checks dates and determines if they're 45 days out, 30 days out, or late

---
+++ Main.py
@@ -0,0 +1,9 @@
+import StudentDates
+
+def main():
+    window: StudentDates.**** = StudentDates.****()
+    window.run()
+
+
+if __name__ == '__main__':
+    main()
--- StudentDates.py
+++ StudentDates.py
@@ -33,7 +33,7 @@ print(SHEET.row_values(1))
 DATE_as_datetime = datetime.datetime(*xlrd.xldate_as_tuple(DATE, WB.datemode))
 print(DATE_as_datetime)
 
-"""New Work -- Driskell"""
+"""16 NOV -- NEW WORK"""
 # Print only date rows
 # for i in range(i + 3, SHEET.ncols):
 #    print(SHEET.cell_value(1, i))
@@ -41,15 +41,27 @@ print(DATE_as_datetime)
 # Above loop overwrites below when active
 
 # Compare rows to current date
-for i in range(i + 3, SHEET.ncols):
+for i in range(i + 1, SHEET.ncols):
     testDate = SHEET.cell_value(1, i) - DATE
-    print(testDate)
-    if testDate <= 45:
-        print("You gonna be late.")
-    else:
-        print("You on time.")
+
+    if SHEET.cell_value(1, i) > DATE:
+        if testDate > 45:
+            print("You are over 45 days out.")
+            print(testDate)
+        elif testDate > 30:
+            print("You are over 30 days out.")
+            print(testDate)
+        elif testDate <= 30:
+            print("You are under 30 days out.")
+            print(testDate)
+    if SHEET.cell_value(1, i) <= DATE:
+        print("You are overdue by: ")
+        print(testDate)
+
+
 # Results are given in days; no conversion necessary
-"""End Of New Work"""
+# Create nested loop to read each row?
+"""16 NOV -- END OF NEW WORK"""
 
 
 class Student:
