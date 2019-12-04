eb1530bc18d4c710048bfc920e2790bd426ad38a
sDriskell <49995352+sDriskell@users.noreply.github.com>
2019-12-04 11:49:30 -0500

    Utility Output Finalized
    Output now provides a header row to label each column's output.  Gives user readability of output's results.

--- Utility.py
+++ Utility.py
@@ -1,3 +1,4 @@
+# Import read/write and date libraries
 import xlrd
 import pathlib
 import datetime
@@ -6,6 +7,7 @@ import xlsxwriter
 
 class Utility:
     def __init__(self):
+        # create the global'ish variables to be used
         self.location = pathlib.Path.cwd() / 'TestExcel.xlsx'
         self.workBook = xlrd.open_workbook(self.location)
         self.sheet = self.workBook.sheet_by_index(0)
@@ -16,6 +18,7 @@ class Utility:
         self.writeName = None
         self.writeDays = None
 
+    # scan through list and create new rows with dates converted to days from today's date
     def read_list(self):
         j = 1
         for j in range(j, self.sheet.nrows):
@@ -24,8 +27,10 @@ class Utility:
             for i in range(i + 3, self.sheet.ncols):
                 test_date = self.sheet.cell_value(j, i) - self.currentDate
                 self.countList[j][i] = test_date
+        # removes header row
         self.countList.pop(0)
 
+    # create columns with student names
     def read_names(self):
         j = 0
         for j in range(j, self.sheet.nrows):
@@ -33,6 +38,7 @@ class Utility:
             for i in range(i, 3):
                 sheet_name = self.sheet.cell_value(j, i)
                 self.nameList[j][i] = sheet_name
+        # removes header row
         self.nameList.pop(0)
 
     def write_list(self):
@@ -42,9 +48,11 @@ class Utility:
         print(self.nameList)
         print(self.countList)
 
-        row = 0
+        row = 1
         col = 0
 
+        """Each "d" is a designator for columns to be written in;
+        d1-3 are empty, rest are floats"""
         for d1, d2, d3, d4, d5, d6, d7, d8, d9, date in self.countList:
             write_worksheet.write(row, col + 0, d1)
             write_worksheet.write(row, col + 1, d2)
@@ -57,16 +65,25 @@ class Utility:
             write_worksheet.write(row, col + 8, d9)
             row += 1
 
-
-        row = 0
+        row = 1
         col = 0
 
+        # writes names to name columns in export
         for last, first, impair in self.nameList:
             write_worksheet.write(row, col+0, last)
             write_worksheet.write(row, col+1, first)
             write_worksheet.write(row, col+2, impair)
             row += 1
 
+        write_worksheet.write(0, 0, "Last")
+        write_worksheet.write(0, 1, "First")
+        write_worksheet.write(0, 2, "Area of Disability")
+        write_worksheet.write(0, 3, "Individual Education Plan")
+        write_worksheet.write(0, 4, "Meeting")
+        write_worksheet.write(0, 5, "Evaluation")
+        write_worksheet.write(0, 6, "Reevaluation")
+        write_worksheet.write(0, 7, "30 Day Test Mark")
+        write_worksheet.write(0, 8, "45 Day Meeting Mark")
 
         write_workbook.close()
 
